85a6df841441a2109ea8f736b7a8433700e05a03
Arnament1007 <pritula1007@gmail.com>
2020-11-10 16:18:41 +0200

    Adding all classes

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -1,4 +1,25 @@
-package PACKAGE_NAME;
+import java.util.ArrayList;
+import java.util.List;
 
-public class main {
+public class Main {
+
+    public static void main(String[] args) {
+
+        InitializationData initializationData = new InitializationData();
+        Information information = initializationData.fillIn();
+
+
+        ArrayList<Integer> integers = new ArrayList<>(); //cntl + alt + v
+
+//        conversion(integers, information);
+        initializationData.comparison(information);
+
+    }
+    private static Information conversion(List<? extends Number> list, Information info){
+        ArrayList<Byte> listByte = new ArrayList<>();
+        ArrayList<Integer> listInteger = new ArrayList<>();
+
+        return  info;
+    }
+
 }
